4aada532ed67da8300ac9f3835381159dfff48b3
Improveiz <aznsuccess@gmail.com>
2022-10-05 12:41:57 -0700

    rng number

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ('hello world')
+from numpy import random
+#import numpy to generate random 
+
+rng=random.randint(0, 20000)
+#variable rng set to radint method w/ stop range to 20k
+
+print(rng)
+#prints rng output 
